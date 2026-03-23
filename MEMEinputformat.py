3b324bdd9be30069849ae98250bcde3385130a52
Carolyn Hicks <chicks6@luc.edu>
2026-03-23 15:21:00 -0500

    meme input format - include edits to increase biological accuracy of letter probability matrix

--- MEMEinputformat.py
+++ MEMEinputformat.py
@@ -1,6 +1,4 @@
 from Bio import SeqIO
-from Bio.Align import substitution_matrices
-import numpy as np
 
 # given fasta files and list of motifs
 # generate correct format for inputs
@@ -22,17 +20,17 @@ def write_motif_file(motifs, out_file):
         f.write("MEME version 5\n")
         f.write(f"ALPHABET= {alphabet}\n\n")
 
-        for i, seq in enumerate(motifs):
+        for i, residue in enumerate(motifs):
             # motif identifier and alternate name
-            f.write(f"MOTIF {seq} motif_{i+1}\n")
+            f.write(f"MOTIF {residue} motif_{i+1}\n")
 
             # letter-probability matrix header
-            f.write(f"letter-probability matrix: alength= {len(alphabet)} w= {len(seq)} nsites= 1\n")
+            f.write(f"letter-probability matrix: alength= {len(alphabet)} w= {len(residue)} nsites= 1\n")
             
             # design probability matrix
             # shows the probabilities of each amino acid at each position in motif
-            # how to include scoring / chemical group similarity? this would modify code to account for weak motifs
-            for i in seq:
+            # this is a strict matrix - edit to account for subsitutions
+            for i in residue:
                 row = []
                 for aa in alphabet:
                     if i == aa:
@@ -41,10 +39,6 @@ def write_motif_file(motifs, out_file):
                         row.append("0.0000")
                 f.write("  " + "  ".join(row) + "\n")
 
-        #blosum62 = substitution_matrices.load("BLOSUM62") 
-        #trimmed_matrix = blosum62.select(alphabet)
-        #f.write(str(trimmed_matrix))
-    
+# test 
 motifs = ["GLALSDLIQKYFF", "LSDLIQ", "LSSLIQ"]
 write_motif_file(motifs, "motifs_input.meme")
-
